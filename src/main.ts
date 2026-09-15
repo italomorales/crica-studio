e94becba5983d99ac440154f18ecfa32214ba9eb
italomorales <italo.morales@hotmail.com>
2026-09-14 21:00:53 -0300

    refactor: disable construction preview feature in AppComponent

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,9 @@ import appTemplate from './app.html?raw';
     template: appTemplate,
 })
 class AppComponent {
-    readonly showConstruction = import.meta.env.PROD || (import.meta.env.DEV && new URLSearchParams(window.location.search).get('preview') === 'construction');
+    // readonly showConstruction = import.meta.env.PROD || (import.meta.env.DEV && new URLSearchParams(window.location.search).get('preview') === 'construction');
+    readonly showConstruction = false;
+    
     router = inject(Router);
     get isAdmin() {
         return this.router.url.startsWith('/admin') || this.router.url.startsWith('/login');
